0a23a40811cf8dc34c09f67859f18dda65c1aa24
John Coburn <johncoburn.it@gmail.com>
2022-04-23 12:58:06 -0400

    Attributes now add dynamically

--- app.js
+++ app.js
@@ -1,67 +1,25 @@
 import { AttributeName, Base, Unique } from './uniqs.js'
+let attrHTMLRows = document.querySelectorAll('#attrColID');
 
-let tempStorage = [];
-function displayAttFields() {
-    let attrDivs = [];
-    let indexer = 0;
-    for (const element of AttributeName.attrArray) {
-        let index = (AttributeName.attrArray.indexOf(element));
+//Creates the rows, columns, and txtarea for editing attribute names
+AttributeName.attrArray.forEach(element => {
 
-        const smallRow = document.createElement("div");
-        
-
-        //Create attrName col
-        const attrNameCol = document.createElement("div");
-        attrNameCol.classList.add("col-2");
-        const attrNameColTextNode = document.createTextNode(`${element._attrName}`)
-        attrNameCol.appendChild(attrNameColTextNode);
-        //Create attrNickName col
-        const attrNickCol = document.createElement("textarea");
-        attrNickCol.classList.add("col-2");
-        attrNickCol.setAttribute("type", "text");
-        attrNickCol.setAttribute("rows", 1);
-        attrNickCol.innerHTML = `${element._attrNickName}`;
-        //Append each to small row
-        smallRow.appendChild(attrNameCol);
-        smallRow.appendChild(attrNickCol);
-        //document.getElementById("containerTestID").innerHTML += "<p>Yoo have append this paragraph " + " times";
-        //Add small row to attrDivs[]
-        attrDivs.push(smallRow);
-        if(indexer >= 6){
-           // appendAttrFields(attrDivs);
-        }
-        appendAttrFields(attrDivs);
-        //TEST append small row to containerAttrListID
-        document.getElementById("containerAttrListID").appendChild(smallRow);
-    }
-}
-
-for(const element in AttributeName.attrArray){
-    let index = (AttributeName.attrArray.indexOf(element)); 
-}
-
-function appendAttrFields(attrDivArray){
-    let _attrDivArray = attrDivArray;
-    let bR = document.createElement("div");
-    bR.classList.add("row");
-
-    for(const element of _attrDivArray){
-        let indexer = _attrDivArray.indexOf(element);
-        //console.log(indexer, element[0].innerText)
-        if((2 % indexer) == 0) {
-           
-            bR = null;
-            bR = document.createElement("div");
-            bR.classList.add("row");
-            bR.classList.add("form-control")
-    };
-        bR.appendChild(element);
-        document.getElementById("containerTest1ID").appendChild(bR);
-    }
-}
-
-//displayAttFields();
-
-
-
-//  containerAttrListID
+    let index = (AttributeName.attrArray.indexOf(element));
+    let row = document.createElement("div");
+    row.classList.add("row");
+    let nameCol = document.createElement("div");
+    nameCol.classList.add("col-6");
+    nameCol.innerText=`${element._attrName}`
+    let nickNameCol = document.createElement("div");
+    nickNameCol.classList.add("col-6");
+    let nickTxtArea = document.createElement("textarea");
+    nickTxtArea.classList.add('w-100');
+    nickTxtArea.setAttribute("rows", 1);
+    nickTxtArea.style.overflow = "hidden";
+    nickTxtArea.innerText=`${element._attrNickName}`
+//need to add listener to textarea
+    nickNameCol.appendChild(nickTxtArea);
+    row.appendChild(nameCol);
+    row.appendChild(nickNameCol);
+    attrHTMLRows[index % 4].appendChild(row);
+});
