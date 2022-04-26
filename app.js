ae8151f88e36123f4eb18d56f48dd0476577cea8
John Coburn <johncoburn.it@gmail.com>
2022-04-26 19:20:17 -0400

    More features and bug fixes.

--- app.js
+++ app.js
@@ -115,10 +115,13 @@ function setAttrNickNames() {
     attrHTMLRows.forEach(element => element.innerHTML = ``);
     //Creates the rows, columns, and txtarea for editing attribute nick names
     exports.AttributeName.attrArray.forEach(element => {
+        //get index
         let index = (exports.AttributeName.attrArray.indexOf(element));
+        //create row for all cols
         let row = document.createElement("div");
         row.style.fontSize = "11px"
         row.classList.add("row");
+        //_attrName col
         let nameCol = document.createElement("div");
         nameCol.classList.add("col-6");
         nameCol.innerText = `${element._attrName}`;
@@ -168,14 +171,11 @@ function addUnique(uniqName) {
     let base = exports.Base.baseArray.filter(function (e) {
         return e._baseName == `${unique[0]._base._baseName}`;
     })
-    // let targetUnique = JSON.parse(JSON.stringify(unique))
     let G = {}
     let B = JSON.parse(JSON.stringify(unique[0]));
     let Z = JSON.parse(JSON.stringify(base[0]))
     Object.assign(G, B, { _base: Z });
-    //Object.assign(g, unique);
     myTradeItems.push(G)
-    //console.log('Added unique to trade list: ', G, `\nCurrent Trade List:`, myTradeItems)
 }
 
 //1.2 removeItem based on myListIndex (modified by click event in textarea)
@@ -230,7 +230,7 @@ function updateTradeList() {
         let name = element._name;
         let isEth = (element._base._isEth) ? ` / Ethereal` : ``;
         let sockets = (element._base._sockets != 0 || element._magicClass == null) ? ` / ${element._base._sockets} OS` : ``;
-        let ed = (element._base._ed != null && element._base._type == 'Armor') ? ` / ${element._base._ed}${exports.AttributeName.attrArray[13]._attrNickName}` : ` / ${element._base._ed}${exports.AttributeName.attrArray[12]._attrNickName}`;
+        let ed = (element._base._ed != null && element._base._type == 'Armor') ? ` / ${element._base._ed}${exports.AttributeName.attrArray[0]._attrNickName}` : ` / ${element._base._ed}${exports.AttributeName.attrArray[1]._attrNickName}`;
         if (element._base._ed == 0) { ed = `` }
         let def = (element._base._type == 'Armor') ? ` / ${calcDefHere(element)} ${exports.AttributeName.attrArray[2]._attrNickName}` : ``;
 
@@ -238,10 +238,11 @@ function updateTradeList() {
         if (element._magicClass == null) { tempString = `${name}${isEth}${sockets}${ed}${def}` }
         //iterate through attribute array
         array.forEach(elementTwo => {
-            (console.log("element two: " ,elementTwo))
+            (console.log("element two: ", elementTwo))
             if (elementTwo._attributeName._attrName != '% Enhanced Defense') {
                 tempString += ` / ${elementTwo._attributeName._attrNickName} ${elementTwo._attrFloorActVal}`;
                 if (elementTwo._attrType == 'skillAttribute') {
+                    alert('hit')
                     tempString += ` / ${elementTwo._classOrTreeName} ${elementTwo._attributeName._attrNickName} ${elementTwo._attrFloorActVal}`;
                 }
                 if (elementTwo._attrType == 'twoFieldAttribute') {
@@ -249,14 +250,11 @@ function updateTradeList() {
                 }
             }
         })
-
-
         tradeThreadTextArea.value += `${tempString}\n`;
 
-        infoWindow.innerText = tempString;
-});
+        //infoWindow.innerText = tempString;
+    });
 }
-
 
 function calcDefHere(element) {
     let ethMultiplier = (element._base._isEth) ? 1.50 : 1.00;
@@ -264,9 +262,9 @@ function calcDefHere(element) {
     let def = parseFloat(element._base._defActVal);
     let addedDef = parseFloat(element._base._addedDef);
     def += addedDef;
-    if(ed != 0){def = def + 1} 
+    if (ed != 0) { def = def + 1 }
     if (ed === null) return Math.floor(def * ethMultiplier);
-    return Math.floor((parseFloat(def  * ethMultiplier)) * ((parseFloat(ed) * 0.01) + 1));
+    return Math.floor((parseFloat(def * ethMultiplier)) * ((parseFloat(ed) * 0.01) + 1));
 }
 
 function sortTradeList() {
@@ -317,6 +315,15 @@ function populateItemBtn() {
         liItem.classList.add("dropdown-item");
         liItem.addEventListener("click", function () {
             chosenName = item;
+            sockRads.forEach(elem => {
+                elem.checked = 'false'
+            })
+            sockRads[0].checked = "true"
+            ethRads.forEach(elem => {
+                elem.checked = 'false'
+            })
+            ethRads[0].checked = "true"
+
             document.getElementById('btnPickItemListID').textContent = `${item}`;
             setEditFields();
         })
@@ -352,44 +359,87 @@ function setEditFields() {
 
 let floorActIndexer = 0;
 function generateRowForField(attr) {
-
+    console.log(attr)
     //1.create row
     const thisRow = document.createElement("div");
     thisRow.classList.add("row");
     thisRow.classList.add("removableAttrRowClass");
     //2.check and create if twofield attribute
     //f aFMinV aFMaxV aFAV
-    //1.1F
-    const attrNickCol = document.createElement("div");
-    attrNickCol.classList.add("col");
-    const attrNickText = document.createTextNode(`${attr._attributeName._attrNickName}`);
-    attrNickCol.appendChild(attrNickText);
-    thisRow.appendChild(attrNickCol)
-    //1.2 aFinV
+    //1.1F 0
+    if (attr._attributeName._attrName != ``) {
+        const attrNickCol = document.createElement("div");
+        attrNickCol.classList.add("col");
+        const attrNickText = document.createTextNode(`${attr._attributeName._attrNickName}`);
+        attrNickCol.appendChild(attrNickText);
+        thisRow.appendChild(attrNickCol)
+    }
+    if (attr instanceof SkillAttribute) {
+        const attrClassOrTreeNameCol = document.createElement("div");
+        attrClassOrTreeNameCol.classList.add("col");
+        const classOrTreeNameTextNode = document.createTextNode(`${attr._classOrTreeName}`);
+        attrClassOrTreeNameCol.appendChild(classOrTreeNameTextNode);
+        thisRow.appendChild(attrClassOrTreeNameCol);
+    }
+    //1.2 aFinV 2
     const attrFloorMinCol = document.createElement("div");
     attrFloorMinCol.classList.add("col");
     const attrFloorMinText = document.createTextNode(`${attr._attrFloorMinVal}`);
     attrFloorMinCol.appendChild(attrFloorMinText);
     thisRow.appendChild(attrFloorMinCol)
-    //1.3 aFMaxV
+    //1.3 aFMaxV 3
     const attrFloorMaxCol = document.createElement("div");
     attrFloorMaxCol.classList.add("col");
-    const attrFloormaxCol = document.createTextNode(`${attr._attrFloorMaxVal}`);
-    attrFloorMaxCol.appendChild(attrFloormaxCol);
+    const attrFloormaxText = document.createTextNode(`${attr._attrFloorMaxVal}`);
+    attrFloorMaxCol.appendChild(attrFloormaxText);
     thisRow.appendChild(attrFloorMaxCol)
-    //1.4 aFAV text area
+    //1.4 aFAV text area 4
     const attrFloorActTextArea = document.createElement("textarea");
     attrFloorActTextArea.classList.add("col-2");
     attrFloorActTextArea.setAttribute("type", "number");
     attrFloorActTextArea.setAttribute("rows", 1);
     attrFloorActTextArea.setAttribute("id", `attrFloorTextAreaID${floorActIndexer}`)
     attrFloorActTextArea.innerHTML = `${attr._attrFloorActVal}`;
+    attrFloorActTextArea.style.resize = "none";
+    attrFloorActTextArea.style.overflow = "hidden";
     attrFloorActTextArea.addEventListener("keyup", (e) => {
         attr._attrFloorActVal = (e.target.value)
         updateCurrentItemInfoWindow();
     })
-    floorActIndexer++;
     thisRow.appendChild(attrFloorActTextArea)
+    if (attr instanceof TwoFieldAttribute) {
+        alert('success')
+        //1.5 aCMinVal
+        const attrACMinValcol = document.createElement("div");
+        attrACMinValcol.classList.add("col");
+        const attrACMinValTextNode = document.createTextNode(`${attr._attrCeilMinVal}`);
+        attrACMinValcol.appendChild(attrACMinValTextNode);
+        thisRow.appendChild(attrACMinValcol);
+        //1.6 aCMaxVal
+        const attrACMaxValcol = document.createElement("div");
+        attrACMaxValcol.classList.add("col");
+        const attrACMaxValTextNode = document.createTextNode(`${attr._attrCeilMaxVal}`);
+        attrACMaxValcol.appendChild(attrACMaxValTextNode);
+        thisRow.appendChild(attrACMaxValcol);
+        //1.7 aCActVal text area
+        const attrCeilActTextArea = document.createElement("textarea");
+        attrCeilActTextArea.classList.add("col-2");
+        attrCeilActTextArea.setAttribute("type", "number");
+        attrCeilActTextArea.setAttribute("rows", 1);
+        attrCeilActTextArea.setAttribute("id", `attrCeilTextAreaID${floorActIndexer}`)
+        attrCeilActTextArea.innerHTML = `${attr._attrFloorActVal}`;
+        attrCeilActTextArea.style.resize = "none";
+        attrCeilActTextArea.style.overflow = "hidden";
+        attrCeilActTextArea.addEventListener("keyup", (e) => {
+            attr._attrCeilActVal = (e.target.value)
+            updateCurrentItemInfoWindow();
+        })
+        thisRow.classList.add("justify-content-center")
+        thisRow.appendChild(attrCeilActTextArea);
+    }
+
+    floorActIndexer++;
+
 
 
 
@@ -429,7 +479,7 @@ for (const radio of ethRads) {
 }
 
 function updateCurrentItemInfoWindow() {
-    
+
     infoWindow.innerText = ``;
     let element = currentItem;
 
@@ -443,18 +493,17 @@ function updateCurrentItemInfoWindow() {
     let name = element._name;
     let isEth = (element._base._isEth) ? ` / Ethereal` : ``;
     let sockets = (element._base._sockets != 0 || element._magicClass == null) ? ` / ${element._base._sockets} OS` : ``;
-    let ed = (element._base._ed != null && element._base._type == 'Armor') ? ` / ${element._base._ed}${exports.AttributeName.attrArray[13]._attrNickName}` : ` / ${element._base._ed}${exports.AttributeName.attrArray[12]._attrNickName}`;
+    let ed = (element._base._ed != null && element._base._type == 'Armor') ? ` / ${element._base._ed}${exports.AttributeName.attrArray[0]._attrNickName}` : ` / ${element._base._ed}${exports.AttributeName.attrArray[1]._attrNickName}`;
     if (element._base._ed == 0) { ed = `` }
     let def = (element._base._type == 'Armor') ? ` / ${calcDefHere(element)} ${exports.AttributeName.attrArray[2]._attrNickName}` : ``;
-    
 
-    
+
+
     tempString += `${name}${isEth}${sockets}${ed}${def}`;
     if (element._magicClass == null) { tempString = `${name}${isEth}${sockets}${ed}${def}` }
     //iterate through attribute array
     let array = element._arr;
     array.forEach(elementTwo => {
-        console.log(elementTwo)
         if (elementTwo._attributeName._attrName != '% Enhanced Defense') {
             tempString += ` / ${elementTwo._attributeName._attrNickName} ${elementTwo._attrFloorActVal}`;
             if (elementTwo._attrType == 'skillAttribute') {
@@ -465,11 +514,45 @@ function updateCurrentItemInfoWindow() {
             }
         }
     })
-    
-
-    console.log('yes we got here')
     infoWindow.innerText = tempString;
-    
+}
+
+
+let body = document.getElementsByTagName('body');
+document.getElementById('moonIconID').addEventListener("click", setNightMode);
+document.getElementById('sunIconID').addEventListener("click",setDayMode);
+
+let isDay = false;
+function checkNightDay() {
+    const isDayJSONStr = localStorage.getItem('dayMode');
+    isDay = JSON.parse(isDayJSONStr);
+    if (isDay) { setDayMode(); }
+    if (!isDay) { setNightMode(); }
+}
+checkNightDay();
+function setDayMode() {
+    body[0].classList.remove("bg-dark")
+    body[0].classList.add('bg-success')
+    body[0].classList.add("bg-gradient")
+    body[0].classList.remove('text-warning')
+    let g = document.getElementsByTagName('textarea');
+    tradeThreadTextArea.classList.remove('bg-dark');
+    tradeThreadTextArea.classList.remove('text-warning');
+    isDay = true;
+    const jsonArr = JSON.stringify(isDay);
+    localStorage.setItem('dayMode', isDay);
+}
+function setNightMode() {
+    body[0].classList.remove("bg-success")
+    body[0].classList.add('bg-dark')
+    body[0].classList.remove("bg-gradient")
+    body[0].classList.add('text-warning')
+    let g = document.getElementsByTagName('textarea');
+    tradeThreadTextArea.classList.add('bg-dark');
+    tradeThreadTextArea.classList.add('text-warning');
+    isDay = false;
+    const jsonArr = JSON.stringify(isDay);
+    localStorage.setItem('dayMode', isDay);
 }
 
 sortTradeList();
